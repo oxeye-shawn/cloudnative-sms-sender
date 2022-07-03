760d34a2d382257fa9828bb07dd05f8251d5045d
Shawn Shchory <shawn@oxeye.io>
2022-07-03 10:09:06 +0300

    Sms sender cloud native app

--- devops/eks-terraform/eks.tf
+++ devops/eks-terraform/eks.tf
@@ -1,20 +1,16 @@
 resource "aws_iam_role" "demo" {
   name = "eks-cluster-demo"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
+  assume_role_policy = jsondecode({
+    "Version": "2012-10-17",
+    "Statement": [{
       "Effect": "Allow",
       "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
+          "Service": "eks.amazonaws.com"
+        },
       "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
